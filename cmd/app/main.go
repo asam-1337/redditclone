ef32f6ecfe7a43c84a5e07b2b9cebfc3cef38af7
asam-1337 <samenkov0280@gmail.com>
2022-10-06 14:59:34 +0300

    add some databse

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,19 +5,30 @@ import (
 	"github.com/asam-1337/reddit-clone.git/internal/repository"
 	"github.com/asam-1337/reddit-clone.git/internal/service"
 	"github.com/asam-1337/reddit-clone.git/pkg/httpserver"
+	"github.com/spf13/viper"
 	"log"
 	"sync"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error occured in initializing config: %s", err.Error())
+	}
+
 	mu := &sync.Mutex{}
 	repo := repository.NewRepository(mu)
 	services := service.NewService(repo)
 	handlers := v1.NewHandler(services)
 
 	srv := new(httpserver.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured in starting server: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
